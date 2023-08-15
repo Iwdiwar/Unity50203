0bffe8b5f12946cf93f5b73e7efc8feaa222d2fe
Iwdiwar <106674354+Iwdiwar@users.noreply.github.com>
2023-08-15 16:48:48 +0300

    start

--- gitvt/Assets/Scripts/Inv_Collected.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Inv_Collected : MonoBehaviour
-{
-    //Имя собираемого объекта
-    public string name;
-    //Картинка собираемого объекта
-    public Sprite image;
-}
